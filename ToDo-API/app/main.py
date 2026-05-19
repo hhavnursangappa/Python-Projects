883b4c715937688df7294a0034dd6473052ec1ec
harish <h.havnursangappa@gmail.com>
2026-05-19 20:02:04 +0200

    Updated the main.py

--- ToDo-API/app/main.py
+++ ToDo-API/app/main.py
@@ -1,6 +1,13 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 from app.database import engine, Base
 from contextlib import asynccontextmanager
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import select
+
+from app.database import engine, Base
+from app.dependencies import get_db
+from app.models import User
+from app.schemas import UserCreate
 
 
 # Create database trables on startup
@@ -16,3 +23,11 @@ app = FastAPI(lifespan=lifespan)
 @app.get("/")
 def root():
     return {"message": "API is running"}
+
+@app.post("/register")
+async def register_user(user: UserCreate, db: AsyncSession = Depends(get_db)):
+    new_user = User(email=user.email, password=user.password)
+    db.add(new_user)
+    await db.commit()
+    await db.refresh(new_user)
+    return {"id": new_user.id, "email": new_user.email}
