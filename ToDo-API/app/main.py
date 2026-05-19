6be37a5170542ec95a56fd424cf87bfe1de56e00
harish <h.havnursangappa@gmail.com>
2026-05-19 19:32:15 +0200

    Updated the main.yp file

--- ToDo-API/app/main.py
+++ ToDo-API/app/main.py
@@ -1,6 +1,17 @@
 from fastapi import FastAPI
+from app.database import engine, Base
+from contextlib import asynccontextmanager
 
-app = FastAPI()
+
+# Create database trables on startup
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+    yield
+    await engine.dispose()
+
+app = FastAPI(lifespan=lifespan)
 
 @app.get("/")
 def root():
